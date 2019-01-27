6746f3c4eff90df1c33189eea876a2c6450a6531
ghankeliu2015 <hanke.liu@gmail.com>
2019-01-26 23:25:59 -0500

    add indes, new, show, edit in view/tools folder for tools controller. add banner iamge for welcome page.

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,11 +1,29 @@
 class ToolsController < ApplicationController
 
-# SHOW
+#NEW
+get "/tools/new" do
+  erb :"tools/new"
+end
 
+#POST
+post "/tools" do
+  @tool = Tool.new(params[:tool])
+  redirect "/tools/:id"
+end
+
+# SHOW
 get "/tools/:id" do
   @tool = Tool.find(params[:id])
   erb :"/tools/show"
 end
+
+#INDEX
+get "/tools" do
+  @tools = Tool.all
+  erb :"/tools/index"
+end
+
+
 
 
 
