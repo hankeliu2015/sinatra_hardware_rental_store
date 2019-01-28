e2bb33a0490ff0a2fd40004d15a495458265f749
ghankeliu2015 <hanke.liu@gmail.com>
2019-01-27 21:51:24 -0500

    finish delete route in tools_controller

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,40 +1,49 @@
 class ToolsController < ApplicationController
 
-#NEW
-get "/tools/new" do
-  erb :"tools/new"
-end
+  #NEW
+  get "/tools/new" do
+    erb :"tools/new"
+  end
 
-#POST
-post "/tools" do
-  @tool = Tool.create(params[:tool])
-  redirect "/tools/#{@tool.id}"
-end
+  #POST
+  post "/tools" do
+    @tool = Tool.create(params[:tool])
+    redirect "/tools/#{@tool.id}"
+  end
 
-# SHOW
-get "/tools/:id" do
-  @tool = Tool.find(params[:id])
-  erb :"/tools/show"
-end
+  # SHOW
+  get "/tools/:id" do
+    @tool = Tool.find(params[:id])
+    erb :"/tools/show"
+  end
 
-#INDEX
-get "/tools" do
-  @tools = Tool.all
-  erb :"/tools/index"
-end
+  #INDEX
+  get "/tools" do
+    @tools = Tool.all
+    erb :"/tools/index"
 
-#EDIT
-get "/tools/:id/edit" do
-  @tool = Tool.find(params[:id])
-  erb :"/tools/edit"
-end
+  end
 
-#PATCH
-patch "/tools/:id" do
-  @tool = Tool.find(params[:id])
-  @tool.update(params[:tool])
-  redirect "/tools/#{@tool.id}"
-end
+  #EDIT
+  get "/tools/:id/edit" do
+    @tool = Tool.find(params[:id])
+    erb :"/tools/edit"
+  end
+
+  #PATCH
+  patch "/tools/:id" do
+    @tool = Tool.find(params[:id])
+    @tool.update(params[:tool])
+    redirect "/tools/#{@tool.id}"
+  end
+
+  #DELETE
+
+  delete "/tools/:id" do
+    @tool = Tool.find(params[:id])
+    @tool.destroy
+    redirect "/tools"
+  end
 
 
 
