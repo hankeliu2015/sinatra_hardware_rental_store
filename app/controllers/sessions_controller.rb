5a16d897b60a8bb88ea6edf8e214acd622a50229
ghankeliu2015 <hanke.liu@gmail.com>
2019-01-29 00:27:15 -0500

    add require flash and register flash in App controller. add sinatra-flash in gemfile. still got undefined flash error. comments out flash[:notice] in sesstion controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,16 +6,18 @@ class SessionsController < ApplicationController
     erb :"/sessions/login"
   end
 
+  #flash[:notice] = "Your user name or password is incorrect, please try again."
+
   post "/login" do
     @customer = Customer.find_by_username_or_email(params[:username_email])
     if @customer && @customer.authenticate(params[:password])
       session[:customer_id] = @customer.id
 
-            #binding.pry
+
       redirect :"/tools"
 
     else
-      # flash.now[:error] = "Your user name or password is incorrect, please try again."
+      binding.pry
       redirect "/login" #add falsh message
     end
   end
