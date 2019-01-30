feee61d8071a6b531ed1c76a9b848fd0efc32c04
ghankeliu2015 <hanke.liu@gmail.com>
2019-01-29 23:29:54 -0500

    add 2 more customers and tools to database from seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,5 +4,11 @@
 # Tool.create(name: "Floor Sander", quantity: 1, rental_date: "", return_date: "", cost_per_day: 50, conditions: "Need repair")
 # Tool.create(name: "Floor Stripper", quantity: 1, rental_date: "", return_date: "", cost_per_day: 65, conditions: "broken")
 
-Customer.create(username: "hank", email: "hank@gmail.com", password: "password")
-Customer.create(username: "henry", email: "henry@gmail.com", password: "password")
+# Customer.create(username: "hank", email: "hank@gmail.com", password: "password")
+# Customer.create(username: "henry", email: "henry@gmail.com", password: "password")
+
+Customer.create(username: "mike", email: "mike@gmail.com", password: "password")
+Customer.create(username: "michael", email: "michael@gmail.com", password: "password")
+
+Tool.create(name: "Floor Polisher", quantity: 1, rental_date: "Jan 29th, 2019", return_date: "Feb 1st, 2019", cost_per_day: 110, conditions: "fair", customer_id: 1)
+Tool.create(name: "Wet Vacuum", quantity: 1, rental_date: "Jan 29th, 2019", return_date: "Feb 2nd, 2019", cost_per_day: 115, conditions: "good", customer_id: 2)
