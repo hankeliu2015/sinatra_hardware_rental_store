a1f1535b6ddfb7dc8d954f7b5b7eea68923e5357
ghankeliu2015 <hanke.liu@gmail.com>
2019-01-30 23:37:13 -0500

    set up routes in customer controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,49 @@
 class CustomerController < ApplicationController
+  #NEW
+  get "/customers/new" do
+    erb :"customers/new"
+  end
+
+  #POST
+  post "/customers" do
+    @customer = Tool.create(params[:customer])
+    redirect "/customers/#{@customer.id}"
+  end
+
+  # SHOW
+  get "/customers/:id" do
+    @customer = Customer.find(params[:id])
+    erb :"/customers/show"
+  end
+
+  #INDEX
+  get "/customers" do
+    if logged_in?               # use helper methos instead of session[:customer_id]
+      @customers = Customer.all
+      erb :"/customers/index"
+    else
+      redirect "/login"
+    end
+  end
+
+  #EDIT
+  get "/customers/:id/edit" do
+    @customer = Customer.find(params[:id])
+    erb :"/customers/edit"
+  end
+
+  #PATCH
+  patch "/customers/:id" do
+    @customer = Customer.find(params[:id])
+    @customer.update(params[:customer])
+    redirect "/customers/#{@customer.id}"
+  end
+
+  #DELETE
+
+  delete "/customers/:id" do
+    @customer = Customer.find(params[:id])
+    @customer.destroy
+    redirect "/customers"
+  end
 end
